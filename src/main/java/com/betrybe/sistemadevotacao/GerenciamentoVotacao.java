ee2ad8c6a6a1a39dc67dd00a2c15fdf22ab1d763
Mariane Candian <mariane.candian@gmail.com>
2023-07-12 11:29:16 -0300

    Gerenciamento de votação

--- src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
@@ -1,6 +1,7 @@
 package com.betrybe.sistemadevotacao;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
 
@@ -19,21 +20,55 @@ public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
 
   @Override
   public void cadastrarPessoaCandidata(String nome, int numero) {
-
+    for (PessoaCandidata pessoa : pessoasCandidatas) {
+      if (pessoa.getNumero() == numero) {
+        System.out.println("Número da pessoa candidata já utilizado!");
+        break;
+      }
+    }
+    PessoaCandidata pessoaCandidata = new PessoaCandidata(nome, numero);
+    pessoasCandidatas.add(pessoaCandidata);
   }
 
   @Override
   public void cadastrarPessoaEleitora(String nome, String cpf) {
-
+    for (PessoaEleitora pessoa : pessoasEleitoras) {
+      if (Objects.equals(pessoa.getCpf(), cpf)) {
+        System.out.println("Pessoa eleitora já cadastrada!");
+        break;
+      }
+    }
+    PessoaEleitora pessoaEleitora = new PessoaEleitora(nome, cpf);
+    pessoasEleitoras.add(pessoaEleitora);
   }
 
   @Override
   public void votar(String cpfPessoaEleitora, int numeroPessoaCandidata) {
-
+    if (cpfsComputados.contains(cpfPessoaEleitora)) {
+      System.out.println("Pessoa eleitora já votou!");
+      return;
+    }
+    for (PessoaCandidata pessoaCandidata : pessoasCandidatas) {
+      if (pessoaCandidata.getNumero() == numeroPessoaCandidata) {
+        pessoaCandidata.receberVoto();
+        cpfsComputados.add(cpfPessoaEleitora);
+        break;
+      }
+    }
   }
 
   @Override
   public void mostrarResultado() {
-
+    int totalVotos = cpfsComputados.size();
+    if (totalVotos > 0) {
+      for (PessoaCandidata pessoa : pessoasCandidatas) {
+        double percentual = Math.round((double) pessoa.getVotos() / totalVotos * 100);
+        System.out.println("Nome: " + pessoa.getNome() + " - " + pessoa.getVotos()
+            + " votos ( " + percentual + "% )");
+      }
+      System.out.println("Total de Votos: " + totalVotos);
+    } else {
+      System.out.println("É preciso ter pelo menos um voto para mostrar o resultado.");
+    }
   }
 }
